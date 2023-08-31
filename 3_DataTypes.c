7a2fe3ef8848d6d3cda3e487f67a4c428176072c
tondeomkar <111636424+tondeomkar@users.noreply.github.com>
2023-08-31 16:10:27 +0530

    Add files via upload

---
+++ 3_DataTypes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int myInt = 55;
+    float myFloat = 1.23;
+    char myChar = 'A';
+
+    printf("Integer: %d\n", myInt);
+    printf("Float: %f\n", myFloat);
+    printf("Character: %c\n", myChar);
+
+    return 0;
+}
