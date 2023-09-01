6cd18a13d1b0393eb4f9cb5cd540565224a378d8
tondeomkar <111636424+tondeomkar@users.noreply.github.com>
2023-09-01 21:08:58 +0530

    Add files via upload

---
+++ Datatypes.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int iNo = 10;
+    float No = 12.020;
+    char ch = 'A';
+    double dNo = 123456;
+
+    printf("This is integer %d\n",iNo);
+    printf("This is float %d\n", No);
+    printf("This is char %c\n", ch);
+    printf("This is double %d\n",dNo);
+
+    return 0;
+}
