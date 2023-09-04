26e6eddee8ab5d0c7ff7cd4170bf192dd3fb5f25
Omkar tonde <111636424+tondeomkar@users.noreply.github.com>
2023-09-04 20:26:57 +0530

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+  int x = 11;
+  int y = 21;
+  if (x > y) {
+    printf("x is greater than y");
+  }  
+  else
+  printf("y is greater than x");
+  return 0;
+}
